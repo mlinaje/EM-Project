df9c273036d041e490b407a636b3d61051d69bd9
Alfonso Galan <algalanb@gmail.com>
2016-01-09 17:41:46 +0100

    added the storage system and fixed some bugs

--- Satellites/nodemcu/nodemcu.ino
+++ Satellites/nodemcu/nodemcu.ino
@@ -2,7 +2,6 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-
 // include the SD library:
 #include <SPI.h>
 #include <SD.h>
@@ -26,25 +25,23 @@ const char* mqtt_server = "192.168.42.1";
 File myFile;
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char status_aux[50];
 char *status_sensors;
 char meta_aux[50];
+char sen_aux[40];
 char *status_meta;
-char json_ip[30];
-int value = 0;
 float h;
 float t;
 String prefix = "Home";
-String nodeID = "nodo_mcu_1";
+String nodeID = "2";
 int capacity = 7753728;
 int busy = 0;
 File root_2;
 int freeSpace = 0;
-
+int local_time = 0;
+int real_time = 0;
+int count = 0;
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   dht.begin(); //Se inicia el sensor
   setup_wifi();
@@ -55,6 +52,7 @@ void setup() {
     return;
   }
    root_2 = SD.open("/");
+   // user_init();
 }
 
 void setup_wifi() {
@@ -68,25 +66,7 @@ void setup_wifi() {
     delay(500);
   }
 }
-void writeSD (char* topic, String msg){
-  myFile = SD.open("test.txt", FILE_WRITE);
-   
-// if the file opened okay, write to it:
-  if (myFile) {
-  // make a string for assembling the data to log:
-  String dataString = "";
-  Serial.println(F("Writting in SD..."));
-  dataString += "Message arrived [";
-  dataString += topic;
-  dataString += "] ";
-  dataString += msg;
-  myFile.println(dataString);
-  myFile.close();
-  } else {
-    Serial.println(F("error opening the file"));
-  }
   
-  }
 void callback(char* topic_in, byte* payload, unsigned int length) {
   
   payload[length] = '\0';
@@ -94,9 +74,10 @@ void callback(char* topic_in, byte* payload, unsigned int length) {
   char payload_char[payload_str.length()+1];
   payload_str.toCharArray (payload_char, payload_str.length()+1);
   String topic_str(topic_in);
+  String nodoTopic = topic_str.substring(topic_str.indexOf("/") + 1 ,topic_str.lastIndexOf("/"));
+  String channel = topic_str.substring(topic_str.lastIndexOf("/") + 1);
   
-  if (topic_str == "/Home/nodo_central/ctrl"){
-    
+  if (channel == "ctrl"){
       StaticJsonBuffer<200> jsonBuffer;
       JsonObject& root = jsonBuffer.parseObject(payload_char);
       
@@ -108,26 +89,68 @@ void callback(char* topic_in, byte* payload, unsigned int length) {
       String nodo_str(nodo);
       const char* op = root["op"];
       String op_str(op);
-          if (nodo_str == "nodo_mcu_1"){
+          if (nodo_str == nodeID){
               if (op_str == "sub"){
-                  client.subscribe("Home/storage");
+                  client.subscribe("Home/+/istate");
               }
               else {
                 if (op_str == "unsub"){
-                  client.unsubscribe("Home/storage");
+                  client.unsubscribe("Home/+/istate");
                 }                 
               }
           }
+    return;
     }
-  if (topic_str == "Home/nodo_mcu_1/request"){
-     client.publish("Home/nodo_mcu_1/reply",payload_char); 
+  if (channel == "request"){
+     client.publish("Home/2/reply",payload_char);
+     return;
   }
-  if (topic_str == "Home/nodo_mcu_1/model_req"){
-     client.publish("Home/nodo_mcu_1/model","{\"mem\":\"Gb\",\"proc\":\"noUnit\",\"batt\":\"mV\"}");  
+    if (channel == "time"){
+     real_time = payload_str.toInt();
+     local_time = millis()/1000;
+     return;
+  }
+  if (channel == "model_req"){
+     client.publish("Home/2/model","{\"nodo\":\"2\",\"time\":\"s\",\"mem\":\"Mb\",\"proc\":\"noUnit\",\"batt\":\"mV\",\"power\":\"noUnit\"}");
+     return; 
   }    
-  else { 
-  //do something
-    }
+  if (channel == "istate")
+  {  
+      StaticJsonBuffer<200> jsonBuffer2;
+      JsonObject& root2 = jsonBuffer2.parseObject(payload_char);    
+      if (!root2.success()) {;
+        return;
+      }
+
+      const char* nodo = root2["nodo"];
+      String nodo_str(nodo);
+      if(nodo_str.length() == 0){
+         String document = "meta_";
+          document = document + nodoTopic;
+          document = document + ".txt";
+          const String &docu = document;    
+         myFile = SD.open(docu, FILE_WRITE);
+          if (myFile) {
+            root2.printTo(myFile);
+            myFile.println();
+            }
+        myFile.close();
+        }
+      else{
+         String document = "model_";
+          document = document + nodoTopic;
+          document = document + ".txt";
+          const String &docu = document; 
+         root2["time"]= getTime();
+
+         myFile = SD.open(docu, FILE_WRITE);
+          if (myFile) {
+            root2.printTo(myFile);
+            myFile.println();
+            }
+            myFile.close();
+      }
+  }
 }
 
 void reconnect() {
@@ -137,21 +160,17 @@ void reconnect() {
     if (client.connect("ESP8266Client")) {
       // ... and resubscribe
       client.subscribe("Home/nodo_central/ctrl");
-      client.subscribe("Home/nodo_mcu_1/request");
-      client.subscribe("Home/nodo_mcu_1/model_req");
-      client.publish("Home/nodo_mcu_1/model","{\"mem\":\"Kb\",\"proc\":\"noUnit\",\"batt\":\"mV\"}");
+      client.subscribe("Home/2/request");
+      client.subscribe("Home/nodo_central/time");
+      client.subscribe("Home/2/model_req");
+      client.publish("Home/2/model","{\"nodo\":\"2\",\"time\":\"s\",\"mem\":\"Gb\",\"proc\":\"noUnit\",\"batt\":\"mV\",\"power\":\"noUnit\"}");
     } else {
       delay(5000);
     }
   }
 }
 
-void updateStatus (String channel, String nodeID, char *message){
- String topicbuff = prefix +"/"+ nodeID +"/"+ channel;
- char topic[topicbuff.length()+1];
- topicbuff.toCharArray (topic,topicbuff.length()+1);
-  client.publish(topic, message);
-  }
+  
 int getFreeSpace(File dir, int numTabs) {
    while(true) {
      
@@ -176,29 +195,57 @@ int leer_voltios(){
 
  return (batLevelInt);
 }
-void checkTempAndHum (){
+
+int getTime(){
+ int timestamp; 
+ int current = millis()/1000;
+ timestamp = real_time+(current-local_time);
+ return (timestamp);
+
+}
+  
+void checkData (){
+  
   h = dht.readHumidity(); //Se lee la humedad
   t = dht.readTemperature(); //Se lee la temperatura
-  long now = millis();
-  now = now/1000;
-  snprintf (status_aux, 50, "{\"Temp\":\"%1d\", \"Hum\":\"%2d\", \"Time\":\"%3d\"}", int (t), int (h), now);
-  status_sensors = status_aux;
+  String msg = "{\"temp\":\"";
+  msg = msg + t;
+  msg = msg + "\",\"tmpU\":\"C\",\"hum\":\"";
+  msg = msg + h;
+  msg = msg + "\",\"humU\":\"%\",\"time\":\"";
+  msg = msg + getTime();
+  msg = msg + "\"}";
+
+  char *cstr = new char[msg.length() + 1];
+  strcpy(cstr, msg.c_str());
+  client.publish("Home/2/istate",cstr);
+  delete [] cstr;
+
+  msg = "{\"mem\":\"";
+  msg = msg + getFreeSpace(root_2,0);
+  msg = msg + "\",\"proc\":\"531\",\"power\":\"0\",\"batt\":\"";
+  msg = msg + leer_voltios();
+  msg = msg + "\"}";
+  char *cstr_1 = new char[msg.length() + 1];
+  strcpy(cstr_1, msg.c_str());
+  client.publish("Home/2/meta",cstr_1);
+  delete [] cstr_1;
   
   }
-void checkMetadata(){
-   snprintf (meta_aux, 50, "{\"mem\":\"%1d\",\"proc\":\"1500\",\"batt\":\"%2d\"}", getFreeSpace(root_2,0),leer_voltios());
-  status_meta = meta_aux;
-  }
+
+
 void loop() {
 
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-   checkTempAndHum();
-   checkMetadata();
-   updateStatus ("istate",nodeID,status_sensors);
-   updateStatus ("meta",nodeID,status_meta);
-   delay (5000);
+  if (count == 100000){
+  checkData ();
+  count = 0;
+  }
+
+  count = count + 1;
+   delay (0);
  
 }
